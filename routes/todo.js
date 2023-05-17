b83b4fa807db580c0f5692f6ee6a296631ed9ee1
jamesberlinlumba <james.lumba@novare.com.hk>
2023-05-17 16:28:12 +0800

    first commit

--- routes/todo.js
+++ routes/todo.js
@@ -1,85 +1,72 @@
 import express from 'express'
-import { z } from 'zod'
+import { isValid, z } from 'zod'
+import { todoCollection } from '../db.js'
+import { ObjectId } from 'mongodb'
 
 const router = express.Router()
 
 const TodoSchema = z.object({
-    id: z.number(),
     task: z.string(),
     completed: z.boolean()
 })
 
-let todos = [
-    {
-        id: 1,
-        task: 'Task 1',
-        completed: false
-    },
-    {
-        id: 2,
-        task: 'Task 2',
-        completed: false
-    },
-    {
-        id: 3,
-        task: 'Task 3',
-        completed: false
-    }
-]
+router.get('/todos', async (req, res) => {
+    const todos = await todoCollection.find().toArray()
 
-router.get('/todos', (req, res) => {
     res.status(200).send(todos)
 })
 
-router.get('/todo/:id', (req, res) => {
-    const todo = todos.find(items => items.id === Number(req.params.id))
+router.get('/todo/:id', async (req, res) => {
+    const id = req.params.id
+    if (!ObjectId.isValid(id)) return res.status(400).send('Invalid ID')
+    const todo = await todoCollection.findOne({_id: new ObjectId(id)})
 
     if (todo) {
         res.status(200).send(todo)
     }
     else {
-        res.status(404).send('not found')
+        res.status(404).send('Page not found')
     }
 })
 
-router.post('/todo', (req, res) => {
-    const todo = { id: new Date().getTime(), ...req.body, completed: false }
+router.post('/todo', async (req, res) => {
+    const todo = { ...req.body, completed: false }
 
     const parsedResult = TodoSchema.safeParse(todo)
     if (!parsedResult.success) {
         return res.status(400).send(parsedResult.error)
     }
 
-    todos = [ ...todos, todo ]
-    res.status(201).send(parsedResult.data)
+    const { acknowledged } = await todoCollection.insertOne(parsedResult.data)
+    if (!acknowledged) {
+        res.status(400).send('Oooops! Something went wrong')
+    }
+    
+    res.status(201).send('Success')
 })
 
-router.patch('/todo/:id', (req, res) => {
-    const todoIndex = todos.findIndex(item => item.id === Number(req.params.id))
-
-    if (todoIndex === -1) {
-        return res.status(404).send('not found')
-    }
-
-    const modifiedTodo = { 'id': Number(req.params.id), ...req.body }
-
-    const parsedResult = TodoSchema.safeParse(modifiedTodo)
+router.patch('/todo/:id', async (req, res) => {
+    const parsedResult = TodoSchema.safeParse(req.body)
     if (!parsedResult.success) {
         return res.status(400).send(parsedResult.error)
     }
 
-    todos[todoIndex] = modifiedTodo
-    res.status(201).send(parsedResult.data)
+    const id = req.params.id
+    if (!ObjectId.isValid(id)) return res.status(400).send('Invalid ID')
+    const todo = await todoCollection.findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: parsedResult.data },
+        { returnNewDocument: true }
+    )
+    
+    res.status(201).send(todo)
 })
 
-router.delete('/todo/:id', (req, res) => {
-    const todoIndex = todos.findIndex(items => items.id === Number(req.params.id))
-    
-    if (todoIndex === -1) {
-        return res.status(404).send('not found')
-    }
+router.delete('/todo/:id', async (req, res) => {
+    const id = req.params.id
+    if (!ObjectId.isValid(id)) return res.status(400).send('Invalid ID')
+    const todo = await todoCollection.findOneAndDelete({ _id: new ObjectId(id) })
 
-    todos.splice(todoIndex, 1);
     res.status(200).send({})
 })
 
